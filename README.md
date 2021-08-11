79f98e8139425adbf9ef7c77156c3fa5cce51427
kirjavascript <snkenjoi@gmail.com>
2021-08-11 23:09:23 +0100

    some cycle testing, definitions

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ support in-game only high level runahead
 sound; use ogg files lol  
 region detection: check pal rom lol  
 responsive DOM gameplay mode  
+anticheat: server side auth  
+HD mode 
--- main.ts
+++ main.ts
@@ -2,12 +2,12 @@ import StateMachineCpu from '6502.ts/lib/machine/cpu/StateMachineCpu';
 import BusInterface from '6502.ts/lib/machine/bus/BusInterface';
 import tetrisROM from './tetris.nes';
 
-console.log(123);
+// foxNES/CTMulator
 
-const CHR = [];
-const PRG = [];
-
-const SRAM = new Uint8Array();
+const header = tetrisROM.slice(0, 0xF);
+const PRG = tetrisROM.slice(0x10, 0x800F);
+const CHR = tetrisROM.slice(0x8010);
+const RAM = new Uint8Array(0x2000);
 
 // arraybuffers for ram
 
@@ -16,10 +16,20 @@ class TetrisBus implements BusInterface {
         console.log('read', address);
         return 0;
     }
-    peek(address: number): number { return 0; };
-    readWord(address: number): number { return 0};
-    write(address: number, value: number): void {};
-    poke(address: number, value: number): void {};
+    peek(address: number): number {
+        console.log('peek', address);
+        return 0;
+    };
+    readWord(address: number): number {
+
+        console.log('readWord', address);
+        return 0};
+    write(address: number, value: number): void {
+        console.log('write', address);
+    };
+    poke(address: number, value: number): void {
+        console.log('poke', address);
+    };
 }
 
 const cpu = new StateMachineCpu(new TetrisBus());
@@ -35,7 +45,15 @@ const cpu = new StateMachineCpu(new TetrisBus());
 
 // TODO: load tetris ROM
 
-console.log(cpu.cycle());
+const loop: () => void = () => requestAnimationFrame(() => {
+    if (!(Date.now() % 10)) {
+        cpu.cycle();
+    }
+    loop();
+});
+loop();
 
 //https://wiki.nesdev.com/w/index.php/Cycle_reference_chart
 // watching
+//
+//
