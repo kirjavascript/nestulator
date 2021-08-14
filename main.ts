6902bdbdefc3d678a0a6478135275358e4168571
kirjavascript <snkenjoi@gmail.com>
2021-08-14 17:12:15 +0100

    some NMI stuff

--- main.ts
+++ main.ts
@@ -15,7 +15,11 @@ let chrPointer = 0;
 const RAM = new Uint8Array(0x2000);
 
 class TetrisBus implements BusInterface {
+    vblank: boolean = false;
+    // frames
+    // nmi enabled
     read(address: number) {
+        // vectors
         if (address === 0xfffa) return 0x05; // nmi
         if (address === 0xfffb) return 0x80;
         if (address === 0xfffc) return 0x0; // initRam
@@ -23,6 +27,7 @@ class TetrisBus implements BusInterface {
         if (address === 0xfffe) return 0x4a; // irq
         if (address === 0xffff) return 0x80;
 
+        // RAM
         if (address < 0x2000) {
             return RAM[address & 0x7ff];
         }
@@ -30,6 +35,18 @@ class TetrisBus implements BusInterface {
         if (address >= 0x8000) {
             return PRG[address - 0x8000];
         }
+
+        if (address === 0x2002) {
+            // PPUSTATUS
+            return this.vblank ? 0x40 : 0;
+        }
+
+        if (address === 0x4016 || address === 0x4017) {
+            // joypad
+            return;
+        }
+
+        console.log(['last', Number(cpu.state.p).toString(16)]);
         console.error('read', address.toString(16));
         return 0;
     }
@@ -48,7 +65,7 @@ class TetrisBus implements BusInterface {
         if (address === 0x2000) {
             // PPUCTRL
             if (value === 0x90) {
-                // NMI enable
+                // TODO: NMI enable
                 // background tile select
             }
             return;
@@ -57,11 +74,19 @@ class TetrisBus implements BusInterface {
             // PPUMASK
             return;
         }
+        if (address === 0x2003) {
+            // OAMADDR
+            return;
+        }
         if (address === 0x2005) {
             // PPUSCROLL, ignore
             return;
         }
-        if (address >= 0x4000 && address <= 0x4017) {
+        if (address === 0x4016) {
+            // joypad
+            return;
+        }
+        if ((address >= 0x4000 && address <= 0x4015) || address === 0x4017) {
             // APU
             return;
         }
@@ -78,50 +103,52 @@ const bus = new TetrisBus();
 const cpu = new StateMachineCpu(bus);
 const disasm = new Disassembler(bus);
 
-// cpu.state.p = 0x4000;
-// cpu._lastInstructionPointer = 0x4000
-
-// interface BusInterface {
-//     read(address: number): number;
-//     peek(address: number): number;
-//     readWord(address: number): number;
-//     write(address: number, value: number): void;
-//     poke(address: number, value: number): void;
-// }
-
 // TODO: vectors
 
 // TODO: load tetris ROM
 // TODO: dump PC each cycle
 
-// TODO: get running at fullspeed, fast
-
 let frames = 0;
+const nmiCycles = 2273;
 
 const interval = setInterval(() => {
-    for (let i = 0; i < 29780; i++) {
-        //  29780 +/- 1
+    const totalCycles = 29780 + (frames & 1) // NTSC
 
+    bus.vblank = false;
+
+    for (let i = 0; i < totalCycles - nmiCycles; i++) {
+        if (cpu.executionState === 1) {
+            // console.log([Number(cpu.state.p).toString(16),  disasm.disassembleAt(cpu.state.p)]);
+        }
+        cpu.cycle();
+        // optimization if (checkForNMI) break
+        // TODO: instead of running x number, of cycles, skip from the rom to vblank
+    }
+
+    cpu.nmi();
+
+    bus.vblank = true;
+
+    for (let i = 0; i < nmiCycles; i++) {
         if (cpu.executionState === 1) {
             // console.log([Number(cpu.state.p).toString(16),  disasm.disassembleAt(cpu.state.p)]);
         }
 
         cpu.cycle();
-        // optimization if (checkForNMI) break
+        // if no nmi, break
     }
-
-    cpu.nmi();
 
     // check interrupts
 
     console.log(cpu.state.p.toString(16));
     debugRAM();
-    // TODO: instead of running x number, of cycles, skip from the rom to vblank
 
-    if (++frames >= 5) {
+    if (++frames > 1) {
         clearInterval(interval);
     }
-}, 1000);
+}, 1);
+
+
 
 const debug = document.body.appendChild(document.createElement('pre'));
 
@@ -130,7 +157,7 @@ function debugRAM() {
     const d = [...RAM];
     for (let cursor = 0; d.length; cursor += 16) {
         lines.push(
-            `0x${(cursor).toString(16).padStart(4, '0')}: ` +
+            `0x${cursor.toString(16).padStart(4, '0')}: ` +
                 d
                     .splice(0, 16)
                     .map((d) => d.toString(16).padStart(2, '0'))
@@ -139,8 +166,3 @@ function debugRAM() {
     }
     debug.innerHTML = `frames: ${frames}\n${lines.join('\n')}`;
 }
-
-// https://wiki.nesdev.com/w/index.php/Cycle_reference_chart
-// watching
-//
-//
